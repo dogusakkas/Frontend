a81ad05f77175b3c8df8dde25f39b5db1c1c4fbe
dogusakkas <dogusakkas@gmail.com>
2021-12-07 16:09:00 +0300

    mobil ve tablet tasarımı

---
+++ js/menu-bar.js
@@ -0,0 +1,19 @@
+//menubar
+
+$("#openmenu").click(function() {
+    $("#mainmenu").toggle("slow");
+});
+
+$("#closemenu").click(function() {
+    $("#mainmenu").hide("fast");
+});
+
+//searchbar
+
+$("#opensearchbar").click(function() {
+    $("#searchbar").toggle(function() {
+        $("#searchbar").animate({
+
+        })
+    })
+});
